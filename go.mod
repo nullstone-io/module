0b654564fc53d6d1be110761e12a06ba54ed2be3
Brad Sickles <brad.sickles@gmail.com>
2020-11-05 19:45:25 -0500

    Built manifest command

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/spf13/afero v1.2.2
 	github.com/stretchr/testify v1.2.2
 	github.com/tmccombs/hcl2json v0.3.1
+	github.com/urfave/cli v1.22.5
 )
--- main.go
+++ main.go
@@ -4,21 +4,52 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"sort"
 
 	"github.com/nullstone-io/module/config"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	files, err := config.ReadDir(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-	cfg, err := config.Parse(files)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var dir string
+	app := &cli.App{
+		Commands: []cli.Command{
+			{
+				Name: "manifest",
+				Description: `This generates a nullstone module manifest from a directory of terraform files.`,
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name: "dir",
+						Usage: "Directory to scan. By default, uses current directory.",
+						Value: ".",
+						Destination: &dir,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					files, err := config.ReadDir(dir)
+					if err != nil {
+						return err
+					}
+					cfg, err := config.Parse(files)
+					if err != nil {
+						return err
+					}
 
-	manifest := cfg.ToManifest()
-	raw, _ := json.Marshal(manifest)
-	fmt.Println(string(raw))
+					manifest := cfg.ToManifest()
+					raw, _ := json.MarshalIndent(manifest, "", "  ")
+					fmt.Println(string(raw))
+					return nil
+				},
+			},
+		},
+	}
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	os.Exit(0)
 }
