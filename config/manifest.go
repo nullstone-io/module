be821602d3b5067774a04b13b3311930f0d20769
Brad Sickles <brad.sickles@gmail.com>
2020-11-06 17:02:54 -0500

    Making manifest serializable to db

--- config/manifest.go
+++ config/manifest.go
@@ -1,10 +1,32 @@
 package config
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+)
+
 type Manifest struct {
 	Providers   []string              `json:"providers"`
 	Connections map[string]Connection `json:"connections"`
 	Variables   map[string]Variable   `json:"variables"`
 	Outputs     map[string]Output     `json:"outputs"`
+}
+
+func (m *Manifest) Value() (driver.Value, error) {
+	return json.Marshal(m)
+}
+
+func (m *Manifest) Scan(src interface{}) error {
+	data, ok := src.([]byte)
+	if !ok {
+		return nil
+	}
+	value := &Manifest{}
+	if err := json.Unmarshal(data, &value); err != nil {
+		return err
+	}
+	*m = *value
+	return nil
 }
 
 type Connection struct {
