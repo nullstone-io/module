f9abff13b7b7d66eb0ac724157becbdbfd8c86de
Brad Sickles <brad.sickles@gmail.com>
2021-07-09 17:21:01 -0400

    Ignoring non-tf files when parsing manifest

---
+++ config/test-fixtures/04/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {}
+data "aws_availability_zone" "this" {}
+
+data "ns_connection" "test" {
+  name = "service"
+  type = "fargate/service"
+}
+
+variable "var1" {
+  type        = string
+  description = "Description for var1"
+  default     = "some-value"
+}
+
+variable "var2" {
+  type        = list(string)
+  description = "Description for var2"
+  default     = ["list", "of", "values"]
+}
+
+output "some_value" {
+  value       = module.network.vpc_id
+  description = "string ||| Some value"
+}
+
+output "sensitive_value" {
+  value       = module.network.vpc_id
+  description = "string ||| "
+  sensitive   = true
+}
+
+output "list_value" {
+  value       = module.network.vpc_id
+  description = "list(string) ||| "
+}
