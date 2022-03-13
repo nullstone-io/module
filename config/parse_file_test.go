e124ed60f524112fbf94e297ba74ace7a232f566
Scott Sickles <scottsickles@gmail.com>
2022-03-12 19:36:26 -0500

    added a fallback test for regular outputs

--- config/parse_file_test.go
+++ config/parse_file_test.go
@@ -12,17 +12,28 @@ func TestParseFiles(t *testing.T) {
 	tests := []struct {
 		name            string
 		outputFile      string
-		expectedEnvVars interface{}
+		expectedOutputs map[string]Output
+		expectedEnvVars map[string]EnvVariable
 	}{
 		{
-			name:            "basic",
+			name:            "parse env and secrets",
 			outputFile:      "05/outputs.tf",
+			expectedOutputs: map[string]Output{},
 			expectedEnvVars: map[string]EnvVariable{
-				"REDSHIFT_USER": { Sensitive: false },
-				"REDSHIFT_DB": { Sensitive: false },
-				"REDSHIFT_PASSWORD": { Sensitive: true },
-				"REDSHIFT_URL": { Sensitive: true },
+				"REDSHIFT_USER":     {Sensitive: false},
+				"REDSHIFT_DB":       {Sensitive: false},
+				"REDSHIFT_PASSWORD": {Sensitive: true},
+				"REDSHIFT_URL":      {Sensitive: true},
 			},
+		},
+		{
+			name:       "fall back to regular outputs",
+			outputFile: "06/outputs.tf",
+			expectedOutputs: map[string]Output{
+				"db_instance_arn":       {Type: "string", Description: "ARN of the Postgres instance", Sensitive: false},
+				"db_master_secret_name": {Type: "string", Description: "The name of the secret in AWS Secrets Manager containing the password", Sensitive: false},
+			},
+			expectedEnvVars: map[string]EnvVariable{},
 		},
 	}
 
